911e94d2ae61a49165beadfa7558bc68d0b3f799
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-05-17 14:29:05 +0000

    linter errors pt. 2

--- results.php
+++ results.php
@@ -9,7 +9,7 @@ $response = "";
 // response for when user does not enter an integer
 if (!is_numeric($integer)) {
     echo "Please enter a valid integer.";
-}
+} 
 
 elseif ($integer == 0) {
     echo "0! --> 1 = 1";
@@ -23,10 +23,10 @@ else {
         $factorial = $factorial * $counter;
       // build the response by using the empty string and adding the factorial with the original integer
         if ($counter == $integer) {
-          $response = $response . $counter;
+            $response = $response . $counter;
         }
         else {
-          $response = $response . " x " . $counter;
+            $response = $response . " x " . $counter;
         }
         // decrement the counter by one
         $counter--;
